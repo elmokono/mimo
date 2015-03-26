475d6dbd5888325179fcc7b18e2a3cf64d4c8990
elmokono <elmokono@hotmail.com>
2015-03-26 14:29:23 -0300

    - added counters for CPU and RAM

--- WindowsPlugin/WinOSPlugin.cs
+++ WindowsPlugin/WinOSPlugin.cs
@@ -4,22 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Diagnostics;
 
 namespace mimo
 {
     public class WinOSPlugin : Plugin
     {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+
         public WinOSPlugin()
             : base("WinOS")
         {
-            var root = new XElement("root",
-                new XElement("item", new XAttribute("Text", "Temperature")),
-                new XElement("item", new XAttribute("Text", "Coolers")),
-                new XElement("item", new XAttribute("Text", "CPU and Memo"))
+            var root = new XElement("root",new XAttribute("ID", "CPUM"), 
+                new XElement("item", new XAttribute("ID", "TEMP"), new XAttribute("Text", "Temperature")),
+                new XElement("item", new XAttribute("ID", "COOL"), new XAttribute("Text", "Coolers")),
+                new XElement("item", new XAttribute("Text", "CPU and Memo"), new XElement("CPUM"))
             );
 
             items = root.Elements();
             currentItem = items.First();
+
+            //counters
+            cpuCounter = new PerformanceCounter()
+            {
+                CategoryName = "Processor",
+                CounterName = "% Processor Time",
+                InstanceName = "_Total",
+            };
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes"); 
         }
 
         public override void Start()
@@ -30,6 +43,10 @@ namespace mimo
         public override string Poll()
         {
             //fill windows info based on current item
+            if (currentItem.Name.LocalName.Equals("CPUM"))
+            {                
+                return String.Format("CPU {0:#0.00}%\r\nRAM {1} MB", cpuCounter.NextValue(), ramCounter.NextValue());
+            }
 
             return base.Poll();
         }
